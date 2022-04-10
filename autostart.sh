76d633ae3ffcbd847058a081954b43e5a6ccebb5
Polar Bear <1097041650@qq.com>
2022-04-11 00:54:42 +0800

    scripts first commit

---
+++ autostart.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+/bin/bash ~/scripts/dwm-status.sh &
+/bin/bash ~/scripts/wp-autochange.sh &
+#picom -o 0.95 -i 0.88 --detect-rounded-corners --vsync --blur-background-fixed -f -D 5 -c -b
+picom -b
+/bin/bash ~/scripts/tap-to-click.sh &
+/bin/bash ~/scripts/inverse-scroll.sh &
+#nm-applet &
+#xfce4-power-manager &
+#xfce4-volumed-pulse &
+#~/scripts/autostart_wait.sh &
---
+++ wp-change.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+feh --recursive --randomize --bg-fill ~/my-wallpapers/
+#feh --recursive --randomize --bg-fill ~/Pictures/wallpapers/view
